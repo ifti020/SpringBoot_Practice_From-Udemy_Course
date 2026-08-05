e9d1f268f9c3c5c6be688170cdfac90fd0eb3e8d
ïfti020 <iftihaque1@gmail.com>
2026-08-05 10:10:00 +0600

    added actuator for helth check

---
+++ UdemyPracticeBoot/01-spring-boot-overview/03-actuator-demo/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,28 @@
+package com.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World From Ifti Haque! <br>" +
+                "Pray for me. I'm working hard to become a Spring Boot Developer.";
+    }
+
+    // exponse a new enfpoint for "workout"
+    @GetMapping("/workout")
+   public String getDailyWorkout(){
+        return "Run a hard 5k";
+    }
+
+ //  expose a new encpoint for "fortune"
+       @GetMapping("/fortune")
+    public String gerDailyFortune(){
+        return "Today is My Lucky day!";
+       }
+
+
+}
