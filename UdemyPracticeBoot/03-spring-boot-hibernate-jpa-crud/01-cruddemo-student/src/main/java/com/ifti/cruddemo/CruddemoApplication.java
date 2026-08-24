03c8b055490988b6baf596d89adbc0df857b81f7
ïfti020 <iftihaque1@gmail.com>
2026-08-24 22:51:04 +0600

    added more 3 students from StudentDAO and it saves at Db

--- UdemyPracticeBoot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ifti/cruddemo/CruddemoApplication.java
+++ UdemyPracticeBoot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/ifti/cruddemo/CruddemoApplication.java
@@ -20,8 +20,23 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner ->
 		{
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+			createMultipeStudents(studentDAO);
 		};
+
+	}
+
+	private void createMultipeStudents(StudentDAO studentDAO) {
+		//create multiple studnets
+		System.out.println("Creating new 3 student object ...");
+		Student tempStudent1 = new Student("Shohan", "Viya", "shohan@gmail.com");
+		Student tempStudent2 = new Student("Shomik", "Viya", "shomik@gmail.com");
+		Student tempStudent3 = new Student("Asif", "Iqbal", "asif@gmail.com");
+		//save this studnet obnjects
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
