6036da035fbe9b52d21137df88f8e7846c1b97d4
ïfti020 <iftihaque1@gmail.com>
2026-08-31 23:20:36 +0600

    rest api with pathVariable

--- UdemyPracticeBoot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/ifti/demo/rest/StudentRestController.java
+++ UdemyPracticeBoot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/ifti/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.ifti.demo.rest;
 
 import com.ifti.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +13,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List <Student> theStudents;
+
+    // define @PostConstruct to load the student data ... only onece!
+
+    @PostConstruct
+    public void loadData()
+    {
+         theStudents = new ArrayList<>();
+        theStudents.add(new Student("Ifti","Haque"));
+        theStudents.add(new Student("Parama","Farhat"));
+        theStudents.add(new Student("Ifsa","Haque"));
+
+    }
+
     // define endpoint for "/student " - return a list of students
 
     @GetMapping("/students")
     public List<Student> getStudents(){
-        List <Student> theStudents = new ArrayList<>();
-        theStudents.add(new Student("Ifti","Haque"));
-        theStudents.add(new Student("Parama","Farhat"));
-        theStudents.add(new Student("Ifsa","Haque"));
+
         return theStudents;
 
+    }
+
+    // define endpoint or "/students/{studentId}"  - return student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId)
+    {
+
+        // just index into the list ... keep it simple for now
+
+        return theStudents.get(studentId);
     }
 }
