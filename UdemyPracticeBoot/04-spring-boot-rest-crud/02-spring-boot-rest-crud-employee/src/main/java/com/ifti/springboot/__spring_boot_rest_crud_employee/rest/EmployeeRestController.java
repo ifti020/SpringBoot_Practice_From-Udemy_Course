92f27dbf7933349b82a0dc755e78a07344dda505
ïfti020 <iftihaque1@gmail.com>
2026-09-05 00:38:33 +0600

    add RestController Only for add employee by id & Find all Employees

--- UdemyPracticeBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ifti/springboot/__spring_boot_rest_crud_employee/rest/EmployeeRestController.java
+++ UdemyPracticeBoot/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ifti/springboot/__spring_boot_rest_crud_employee/rest/EmployeeRestController.java
@@ -2,9 +2,8 @@ package com.ifti.springboot.__spring_boot_rest_crud_employee.rest;
 
 import com.ifti.springboot.__spring_boot_rest_crud_employee.entity.Employee;
 import com.ifti.springboot.__spring_boot_rest_crud_employee.service.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +25,27 @@ public class EmployeeRestController {
     public List<Employee> findAll(){
         return  employeeService.findAll();
     }
+
+    //add mapping for get / employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee == null ){
+            throw new RuntimeException("Employee with id " + employeeId + " not found");
+        }
+        return theEmployee;
+    }
+
+    // add mapping for post/ employees - add new employee
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        // also just in case they pass an id in JSON... set id to 0
+        // this is to force a save of new item ... instead of update
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
 
 }
