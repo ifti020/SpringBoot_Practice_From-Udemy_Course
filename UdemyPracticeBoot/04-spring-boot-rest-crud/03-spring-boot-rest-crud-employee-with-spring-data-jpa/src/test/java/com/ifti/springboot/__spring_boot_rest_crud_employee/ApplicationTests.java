49c5a8a29c1b69eea2139e4e05aab581d9c26fd0
ïfti020 <iftihaque1@gmail.com>
2026-09-05 22:18:14 +0600

    Implemented Spring data jpa for Spring Rest Crud Using Data Jpa for Employee 'spring jpa give us magic crud all methods free zerocoding'

---
+++ UdemyPracticeBoot/04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/test/java/com/ifti/springboot/__spring_boot_rest_crud_employee/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifti.springboot.__spring_boot_rest_crud_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
