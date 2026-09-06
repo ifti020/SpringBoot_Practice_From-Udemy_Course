5d0d2fa8dbbc0dbe78e30bd81ed49bb5dcaa3c98
ïfti020 <iftihaque1@gmail.com>
2026-09-06 13:33:16 +0600

    Use OpenApi Swagger for test RestApi instead of PostMan. Also configure Custom path: '/my-ui.html' & '/my-api-doc'

---
+++ UdemyPracticeBoot/04-spring-boot-rest-crud/06-openapi-swagger-crud-employee/src/main/java/com/ifti/springboot/__spring_boot_rest_crud_employee/rest/EmployeeRestController.java
@@ -0,0 +1,108 @@
+package com.ifti.springboot.__spring_boot_rest_crud_employee.rest;
+
+import com.ifti.springboot.__spring_boot_rest_crud_employee.entity.Employee;
+import com.ifti.springboot.__spring_boot_rest_crud_employee.service.EmployeeService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    private JsonMapper jsonMapper;
+
+    // quick and dirty : inejct employee dao
+
+    public EmployeeRestController(EmployeeService theEmployeeService, JsonMapper theJsonMapper){
+        this.employeeService= theEmployeeService;
+        this.jsonMapper = theJsonMapper;
+    }
+
+    // expose "/employees" and return a list of employees
+
+    @GetMapping("/employees")
+    public List<Employee> findAll(){
+        return  employeeService.findAll();
+    }
+
+    //add mapping for get / employees/{employeeId}
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+        if(theEmployee == null ){
+            throw new RuntimeException("Employee with id " + employeeId + " not found");
+        }
+        return theEmployee;
+    }
+
+    // add mapping for post/ employees - add new employee
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        // also just in case they pass an id in JSON... set id to 0
+        // this is to force a save of new item ... instead of update
+        theEmployee.setId(0);
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+
+    // add mapping for put/employees - update exixting employees
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee)
+    {
+        Employee dbEmployee = employeeService.save(theEmployee);
+        return dbEmployee;
+    }
+    //
+    
+    //add mapping for patch
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId,
+                                  @RequestBody Map<String, Object> patchPayload){
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if(tempEmployee == null)
+        {
+            throw new RuntimeException("Employee id not found " + "-  " + employeeId );
+        }
+
+        // throw exception if request body contains "id" key
+        if(patchPayload.containsKey("id")){
+            throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+        }
+
+        Employee patchedEmployee = jsonMapper.updateValue(tempEmployee, patchPayload);
+
+        Employee dbEmployee = employeeService.save(patchedEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for DELETE /employees/{employeeId} - delete employee
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee (@PathVariable int employeeId)
+    {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+
+        if(tempEmployee == null )
+        {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        employeeService.deleteById(employeeId);
+        return "Deleted Employee Id - " + employeeId;
+
+    }
+
+
+}
