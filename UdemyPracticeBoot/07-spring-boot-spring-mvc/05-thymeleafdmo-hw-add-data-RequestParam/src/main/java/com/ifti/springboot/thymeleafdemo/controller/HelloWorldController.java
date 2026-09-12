7dc74da9dc074daac3526c00b473be73abc392b4
ïfti020 <iftihaque1@gmail.com>
2026-09-13 00:54:43 +0600

    Reading html form data with @Request Param Annotaiton

---
+++ UdemyPracticeBoot/07-spring-boot-spring-mvc/05-thymeleafdmo-hw-add-data-RequestParam/src/main/java/com/ifti/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,62 @@
+package com.ifti.springboot.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Locale;
+
+@Controller
+public class HelloWorldController {
+
+
+    // step 1 : need a controller method to show initial HTML form
+    // step 2 : need a controller method to process the HTML
+
+    @RequestMapping("/showForm")
+    public String showForm ()
+    {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm()
+    {
+        return "helloworld";
+    }
+
+    // 04 -mvc-hellowrold
+    // step 1 : need a controller method to read form data and
+    // step 2 : add data to the model
+
+    @RequestMapping("/processFormV2")
+    public String letsShoutDube(HttpServletRequest request, Model model) {
+        // read the request parameter from the HTML form
+        // convert the data to all caps
+        // create the message
+        // add message to the model
+
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "YO! " + theName;
+        model.addAttribute("message", result);
+
+        return "helloworld";
+
+    }
+
+        // add @RequestParam for our controller
+        @RequestMapping("/processFormV3")
+        public String processFormV3(@RequestParam("studentName") String theName, Model model) {
+
+            theName = theName.toUpperCase();
+            String result = "Hey My Friend from @RequesrParam v3! " + theName;
+            model.addAttribute("message", result);
+
+            return "helloworld";
+        }
+}
